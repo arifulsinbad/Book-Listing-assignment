5aed4efbd5667f857555c545351ff241c7af2de8
unknown <arifulsinbad>
2023-09-03 05:21:43 +0600

    first commit8

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import { UserRoute } from '../modules/user.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/auth',
+    routes: UserRoute,
+  },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;
