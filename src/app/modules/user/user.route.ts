b8525da07902435928be8da4b0a7b44d8addcaf2
unknown <arifulsinbad>
2023-09-04 22:43:25 +0600

    first commit18

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
-router.post('/signup', UserController.insertIntoDB);
+router.post(
+  '/signup',
+  validateRequest(UserValidation.create),
+  UserController.insertIntoDB
+);
 router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllFromDB);
 router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleFromDB);
 router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteFromDB);
